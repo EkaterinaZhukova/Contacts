e4d6ed8f96e99ab2d4dcc2e0099cc58bbe71838d
EkaterinaZhukova <kate.zhukova.98@gmail.com>
2018-11-11 13:49:36 +0300

    Initial Commit

---
+++ ContactsTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsTask
+//
+//  Created by Екатерина on 11/11/18.
+//  Copyright © 2018 Екатерина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
